c955c43aa9afbd301dbfca5ea6d049886ee42d97
hasanqusay <hasanqusay@yahoo.com>
2016-05-12 00:36:32 -0500

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace facialrecon
+{
+    partial class FrmPrincipal
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBoxFrameCapture = new Emgu.CV.UI.ImageBox();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.label2);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Controls.Add(this.button1);
+            this.groupBox.Location = new System.Drawing.Point(524, 18);
+            this.groupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox.Size = new System.Drawing.Size(314, 372);
+            this.groupBox.TabIndex = 9;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Results: ";
+            this.groupBox.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(259, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 22);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Number of faces detected: ";
+            this.label1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(75, 167);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 82);
+            this.button1.TabIndex = 2;
+            this.button1.Text = " Detect Face";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBoxFrameCapture
+            // 
+            this.imageBoxFrameCapture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBoxFrameCapture.Location = new System.Drawing.Point(18, 18);
+            this.imageBoxFrameCapture.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.imageBoxFrameCapture.Name = "imageBoxFrameCapture";
+            this.imageBoxFrameCapture.Size = new System.Drawing.Size(479, 368);
+            this.imageBoxFrameCapture.TabIndex = 4;
+            this.imageBoxFrameCapture.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 446);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.imageBoxFrameCapture);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmPrincipal";
+            this.Text = "Face Detector";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameCapture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Emgu.CV.UI.ImageBox imageBoxFrameCapture;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+

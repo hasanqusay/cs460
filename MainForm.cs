5f6f10a9d78e53f5d142f684a8befd3b1f1e3f3f
hasanqusay <hasanqusay@yahoo.com>
2016-05-12 01:05:29 -0500

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Diagnostics;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
@@ -11,8 +11,7 @@ using Emgu.CV.CvEnum;
 
 namespace facialrecon
 {
-    public partial class FrmPrincipal:Form
-    {
+    public partial class FrmPrincipal:Form {
         Image<Bgr, Byte> Frame;
         Capture capture;    
         HaarCascade face;
@@ -21,23 +20,21 @@ namespace facialrecon
         Image<Gray, byte> grayFrame = null;
         int t;
 
-
         public FrmPrincipal() {
             InitializeComponent();
             face = new HaarCascade("haarcascade_frontalface_default.xml");
         }
 
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            capture = new Capture();
+        private void button1_Click(object sender, EventArgs e) {
+            capture= new Capture();
             capture.QueryFrame();
-            Application.Idle += new EventHandler(FrameCapture);
+            Application.Idle+= new EventHandler(FrameCapture);
+            
             //disable the button after clicking
             button1.Enabled = false;
         }
-        void FrameCapture(object sender, EventArgs e)
-        {
+        
+        void FrameCapture(object sender, EventArgs e) {
             label2.Text = "0";
 
             Frame = capture.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
@@ -48,22 +45,20 @@ namespace facialrecon
             //Detect All Faces
             MCvAvgComp[][] faces = grayFrame.DetectHaarCascade(face, 1.2, 10,Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
 
-                    //Action for each element detected
-            foreach (MCvAvgComp f in faces[0])
-            {
+            //Detect each face in the camera and draw a rectangle around it 
+            foreach (MCvAvgComp f in faces[0]) {
+                //Add up the number of faces
                 t = t + 1;
                 result = Frame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
 
                 //Draw a rectangle on face detected
-                Frame.Draw(f.rect, new Bgr(Color.Red), 2);
-                label2.Text = faces[0].Length.ToString();
+                Frame.Draw(f.rect, new Bgr(Color.Blue), 2);
+                label2.Text= faces[0].Length.ToString();
 
             }
             t = 0; 
             imageBoxFrameCapture.Image = Frame;
-
         }
-
         
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
